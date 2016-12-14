11ecc60a282a976bd9190a1f0945d617f8d6e608
caiyou <rodickcai@gmail.com>
2016-12-14 20:49:23 +0800

    upd onerror

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: caiyou
 * @Date:   2016-12-14 15:11:11
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-12-14 20:38:12
+* @Last Modified time: 2016-12-14 20:47:03
 */
 
 'use strict'
@@ -11,11 +11,13 @@ let app = require('koa')(),
   co = require('co'),
   router = require('koa-router')(),
   bodyParser = require('koa-bodyparser'),
+  onerror = require('koa-onerror'),
   controllers = require('./controllers/'),
   utils = require('./utils/'),
   debug = utils.debug
 
 co(function * () {
+  onerror(app)
 
   app.use(bodyParser())
 
