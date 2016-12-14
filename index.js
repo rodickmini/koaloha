d6b94511b63d22c455bc130935decc93ff963c67
caiyou <rodickcai@gmail.com>
2016-12-14 20:41:28 +0800

    upd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: caiyou
 * @Date:   2016-12-14 15:11:11
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-12-14 20:24:46
+* @Last Modified time: 2016-12-14 20:38:12
 */
 
 'use strict'
@@ -12,7 +12,9 @@ let app = require('koa')(),
   router = require('koa-router')(),
   bodyParser = require('koa-bodyparser'),
   controllers = require('./controllers/'),
-  utils = require('./utils/')
+  utils = require('./utils/'),
+  debug = utils.debug
+
 co(function * () {
 
   app.use(bodyParser())
