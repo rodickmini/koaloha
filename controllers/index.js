94297407964ba6dc62f1c68be1f7547d1a17b306
caiyou <rodickcai@gmail.com>
2016-12-15 12:25:11 +0800

    upd controller require注释

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@
 * @Author: caiyou
 * @Date:   2016-12-14 17:06:45
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-12-14 20:01:35
+* @Last Modified time: 2016-12-15 12:24:52
 */
 
 'use strict'
@@ -12,6 +12,9 @@ const debug = utils.debug
 const path = require('path')
 const fs = require('fs')
 
+/**
+* require当前目录下除了index.js之外所有的js文件，然后传入router参数执行其init方法
+*/
 module.exports.init = (router) => {
   let files = fs.readdirSync(__dirname)
   files.forEach((item) => {
