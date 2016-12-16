f935b1cdbd483095f615a60feb69dc6deb1dc013
caiyou <rodickcai@gmail.com>
2016-12-16 14:58:42 +0800

    upd 站点配置页面

--- controllers/site.js
+++ controllers/site.js
@@ -2,7 +2,7 @@
 * @Author: caiyou
 * @Date:   2016-12-14 17:06:12
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-12-16 14:13:26
+* @Last Modified time: 2016-12-16 14:52:32
 */
 
 'use strict'
@@ -13,10 +13,16 @@ const config = require('../config')
 
 module.exports.init = (router) => {
   router.get('/install', showInstallPage)
+  router.post('/siteinfo', configSiteInfo)
 }
 
 function* showInstallPage() {
   yield this.render('install', {
     test: '哈哈哈哈哈哈'
   })
+}
+
+function* configSiteInfo() {
+  debug('config site info...')
+  debug('this.request.body: %o', this.request.body)
 }
