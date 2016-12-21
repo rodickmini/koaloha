22f29ce206cc44fb1b3cc848fe26787d7dc148e0
caiyou <rodickcai@gmail.com>
2016-12-21 15:07:17 +0800

    generate write page

---
+++ client/src/components/Write.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <hello/>
+    <div class="write-container">
+      <textarea class="write-space">#markdown here</textarea>
+      <div class="result-space"></div>
+    </div>
+  </div>
+</template>
+<script>
+  let Hello = require('components/Hello')
+  export default {
+    name: 'write',
+    components: {Hello}
+  }
+</script>
+<style lang="stylus" scoped>
+.write-container
+  padding 20px
+  textarea.write-space
+    box-sizing border-box
+    float left
+    width 50%
+    height 500px
+    padding 10px
+    background-color #2d2d2d
+    color #ccc
+    outline none
+    border none
+    resize none
+  .result-space
+    width 50%
+    float left
+</style>
--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@
 * @Author: caiyou
 * @Date:   2016-12-16 18:03:57
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-12-20 15:15:14
+* @Last Modified time: 2016-12-21 14:53:27
 */
 
 'use strict'
@@ -10,11 +10,13 @@ import Index from 'components/Index'
 import Hello from 'components/Hello'
 import Detail from 'components/Detail'
 import Login from 'components/Login'
+import Write from 'components/Write'
 
 export default {
   routes: [
     {path: '/', component: Index},
     {path: '/detail', component: Detail},
     {path: '/login', component: Login},
+    {path: '/write', component: Write}
   ]
 }
