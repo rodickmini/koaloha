3510855e9d53b8c0337f1f3dabc5f164a125745c
caiyou <rodickcai@gmail.com>
2016-12-21 18:58:22 +0800

    upd fullscreen icon

--- client/src/components/Write.vue
+++ client/src/components/Write.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <hello/>
-    <div class="write-container">
+    <div class="write-container" v-bind:class="{fullscreen: isFullscreen}">
+      <span class="toggle-fs-btn" title="full screen" @click="fullscreen" v-html="fullScreenIcon"></span>
       <textarea class="write-space" v-model="inputContent"></textarea>
       <div class="result-space" v-html="outputContent"></div>
     </div>
@@ -17,22 +18,34 @@
     data() {
       return {
         inputContent: '# markdown here',
+        isFullscreen: false
       }
     },
     computed: {
       outputContent: function() {
         return marked(this.inputContent)
+      },
+      fullScreenIcon: function() {
+        return this.isFullscreen ? '><' : '<>'
+      }
+    },
+    methods: {
+      fullscreen: function() {
+        this.isFullscreen = !this.isFullscreen
       }
     }
   }
 </script>
 <style lang="stylus" scoped>
 @import 'nib'
+@import '../assets/stylus/variables.styl'
 .write-container
-  height 500px
   clearfix()
-  relative()
+  fixed top 280px right 0 bottom 0 left 0
   z-index 1
+  transition: all .6s ease
+  &.fullscreen
+    fixed top 0 right 0 bottom 0 left 0
   .write-space
     box-sizing border-box
     float left
@@ -52,4 +65,11 @@
     float left
     padding 10px
     background-color #f1f1f1
+  .toggle-fs-btn
+    absolute top 0 right 50%
+    padding 0 10px
+    color $themeColor
+    font-size 30px
+    cursor pointer
+    transform rotate(90deg)
 </style>
