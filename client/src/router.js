8ac202f951ea927a92a22d433d2403973529c98e
caiyou <rodickcai@gmail.com>
2016-12-23 17:16:17 +0800

    redirect user to where he from while he has signed in

--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@
 * @Author: caiyou
 * @Date:   2016-12-16 18:03:57
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-12-23 17:10:56
+* @Last Modified time: 2016-12-23 17:14:59
 */
 
 'use strict'
@@ -16,7 +16,16 @@ export default {
   routes: [
     { path: '/', component: Index },
     { path: '/detail/:id', component: Detail },
-    { path: '/signin', component: Signin },
+    { path: '/signin', component: Signin, 
+      beforeEnter: (to, from, next) => {
+        let jwtToken = localStorage.getItem('koaloha_token')
+        if(jwtToken === null) {
+          next()
+        }else {
+          next(from.path)
+        }
+      }
+    },
     { path: '/write', component: Write,
       beforeEnter: (to, from, next) => {
         let jwtToken = localStorage.getItem('koaloha_token')
