acc34f260b57b4180a7671da02c51a8031b2a79a
caiyou <rodickcai@gmail.com>
2016-12-23 12:20:07 +0800

    upd who wrote x ago

--- client/src/components/Detail.vue
+++ client/src/components/Detail.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <hello/>
-    <div class="content" v-html="articleMarked"></div>
+    <div class="article-detail">
+      <div class="article-meta">
+        <span>{{article.author}} 写于 <span :title="new Date(+article.createTime)">{{+article.createTime | nicedate}}</span></span>
+      </div>
+      <div class="content" v-html="articleMarked"></div>
+    </div>
   </div>
 </template>
 
@@ -14,6 +19,7 @@ export default {
   components: {Hello},
   data () {
     return {
+      article: '',
       articleMarked: ''
     }
   },
@@ -21,18 +27,42 @@ export default {
     let articleId = this.$route.params.id
     articleService.getDetail(articleId).then((r) => {
       if(r.code === 0) {
+        this.article = r.data.article
         this.articleMarked = marked(r.data.article.content)
       }
     }).catch((err) => {
       alert(err.err_msg)
     })
+  },
+  filters: {
+    nicedate: function(value) {
+      let nicedate = require('nicedate')
+      try {
+        return nicedate(value, true, 'zh')
+      }catch (e) {
+        console.log(e)
+      }
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.content
+@import 'nib'
+.article-detail
+  relative()
   margin 0 auto 230px
   max-width 750px
   text-align left
+  &:hover
+    .article-meta
+      opacity 1
+
+.article-meta
+  absolute right 0
+  line-height 42px
+  font-size 12px
+  color #aaa
+  opacity 0
+  transition all .6s
 </style>
--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -4,7 +4,7 @@
       <div class="content">
         <h1>
           <router-link :to="'/detail/' + article._id">{{article.title}}</router-link>
-          <span class="katime" :title="article.createTime">{{article.createTime}}</span>
+          <span class="article-meta">{{article.author}} 写于 <span :title="new Date(+article.createTime)">{{+article.createTime | nicedate}}</span></span>
         </h1>
         <p>{{article.abstract}}</p>
       </div>
@@ -30,6 +30,17 @@
       }).catch((err) => {
         alert(err.err_msg)
       })
+    },
+    filters: {
+      nicedate: function(value) {
+        //ref: https://github.com/ssbc/nicedate
+        let nicedate = require('nicedate')
+        try {
+          return nicedate(value, true, 'zh')
+        }catch (e) {
+          console.log(e)
+        }
+      }
     }
   }
 </script>
@@ -47,9 +58,9 @@
       font-size 1.5em
       margin-bottom 0
       letter-spacing 2px
-      .katime
+      .article-meta
         font-size 12px
-        color #bbb
+        color #aaa
         font-weight normal
     p
       margin-top 0
