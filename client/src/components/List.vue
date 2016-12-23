7aebf45ec7951ebe2f0253664d4933ce1bf61cc4
caiyou <rodickcai@gmail.com>
2016-12-23 12:53:27 +0800

    markdown abstract

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -6,7 +6,7 @@
           <router-link :to="'/detail/' + article._id">{{article.title}}</router-link>
           <article-meta :author="article.author" :timestamp="article.createTime"></article-meta>
         </h1>
-        <p>{{article.abstract}}</p>
+        <p v-html="article.abstract_marked"></p>
       </div>
     </section>
   </div>
@@ -14,6 +14,7 @@
 <script>
   import articleService from "../services/article"
   let ArticleMeta = require('components/ArticleMeta')
+  let marked = require('marked')
   export default {
     name: 'list',
     components: {ArticleMeta},
@@ -27,7 +28,10 @@
         start: 0, limit: 10
       }).then((r) => {
         if(r.code === 0) {
-          this.articleList = r.data.articles
+          this.articleList = r.data.articles.map((articleObj) => {
+            articleObj.abstract_marked = marked(articleObj.abstract)
+            return articleObj
+          })
         }
       }).catch((err) => {
         alert(err.err_msg)
