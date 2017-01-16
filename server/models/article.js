afaac3f558a104e0c964e2c8352fda402ee3165b
caiyou <rodickcai@gmail.com>
2017-01-16 15:19:16 +0800

    upd article schema add lastUpdateTime, isDeleted, pv

--- server/models/article.js
+++ server/models/article.js
@@ -2,7 +2,7 @@
 * @Author: caiyou
 * @Date:   2016-12-15 20:44:42
 * @Last Modified by:   caiyou
-* @Last Modified time: 2016-12-23 10:45:06
+* @Last Modified time: 2017-01-16 15:18:37
 */
 
 'use strict'
@@ -16,7 +16,10 @@ ArticleSchema = new mongoose.Schema({
   content: String,
   author: String,
   createTime: String,
-  tags: [String]
+  lastUpdateTime: String,
+  tags: [String],
+  isDeleted: { type: Boolean, default: false },
+  pv: { type: Number, default: 0 }
 })
 
 ArticleModel = mongoose.model('Article', ArticleSchema)
